b07f3ff974eac7dabe2b528b13e37249995a1ca9
bhartinderjoshi <104310508+bhartinderjoshi@users.noreply.github.com>
2023-10-05 21:06:44 +0530

    Add files via upload

---
+++ arrow_waypoints.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class arrow_waypoints : MonoBehaviour
+{
+    public Transform target;
+    public float arrowspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 relativePos = target.position - transform.position;
+
+        
+        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
+        transform.rotation = rotation;
+
+    }
+}
